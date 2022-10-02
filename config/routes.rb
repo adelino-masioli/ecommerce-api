439008076174e4915d03b944182f6bddcaec85f4
Adelino Masioli <adelinomasioli@Adelinos-MBP.home>
2022-10-02 12:36:51 +0100

    Create concern Authenticatable

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
+
+  namespece :admin do
+    namespece :v1 do
+    end
+  end
+
+  namespece :storefront do
+    namespece :v1 do
+    end
+  end
 end
